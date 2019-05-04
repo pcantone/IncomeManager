274918b5f54cf18d8d4442049a3b43bc3d1d6944
Michael Palumbo <mp3492@drexel.edu>
2019-05-04 14:47:31 -0400

    The beginning of event manager

--- Main/script.js
+++ Main/script.js
@@ -1,16 +1,40 @@
 
 
+var eventList = []
+var currentEvent = null
+
 function init(){
+  currentEvent = new event(tempPrompt,tempChoices,tempConsequence);
   personalAttr = new phaseTwo();
 }
 
 /* Event Class */
-function event(){
-  this.prompt = ""
+function event(prompt, choices, con){
+  this.prompt = prompt;
+  this.choices = choices;
+  this.consequence = con;
+}
 
-  /* List of Choices */
-  this.choices = []
+tempPrompt = "Yard sale day is coming up, you have the opportunity to get rid of some old items:"
 
-  /* List of Functions */
-  this.consequence   = []
+/* List of Choices */
+tempChoices = [
+  "Spend a few hours selling old items",
+  "Hold onto your goods",
+  "Do another thing that I couldn’t think of an option for",
+]
+
+/* List of Functions */
+tempConsequence = [
+  () => {setHealth(-10)},
+  () => {setMoney(-50) },
+  () => {  },
+]
+
+currentEvent = new event(tempPrompt,tempChoices,tempConsequence);
+
+var eventList = []
+
+function clickeChoice( indexOfChoice ){
+  currentEvent.consequence[indexOfChoice]();
 }
