08e6848b68afb2563364c5c06d7a0cf01a69ab52
Nathan Beebe <njb99@drexel.edu>
2019-05-04 14:38:36 -0400

    phaseTwo
    getters and setter for phaseTwo

---
+++ Main/phase_two.js
@@ -0,0 +1,40 @@
+
+
+function init(){
+
+}
+/* Person Attribute Class */ 
+function phaseTwo(){
+    this.health = 100;
+    this.money = 100;
+    this.hunger = 100;
+    this.happiness = 100;
+}
+
+/* Getter Functions*/ 
+function getHealth(){
+    return phaseTwo.health();
+}
+function getMoney(){
+    return phaseTwo.money();
+}
+function getHunger(){
+    return phaseTwo.hunger();
+}
+function getHappyness(){
+    return phaseTwo.happiness();
+}
+
+/* Setter Functions */
+function setHealth(num){
+    return phaseTwo.health() + num;
+}
+function setMoney(num){
+    return phaseTwo.money() + num;
+}
+function setHunger(num){
+    return phaseTwo.hunger + num;
+}
+function setHappyness(num){
+    return phaseTwo.happiness() + num;
+}
--- Main/script.js
+++ Main/script.js
@@ -1,7 +1,7 @@
 
 
 function init(){
-  
+  personalAttr = new phaseTwo();
 }
 
 /* Event Class */
