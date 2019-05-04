d6ef6e008d2501fa2e6ed635f2bdf099cc951e51
Michael Palumbo <mp3492@drexel.edu>
2019-05-04 13:44:19 -0400

    Layout of Index

---
+++ Main/script.js
@@ -0,0 +1,16 @@
+
+
+function init(){
+  
+}
+
+/* Event Class */
+function event(){
+  this.prompt = ""
+
+  /* List of Choices */
+  this.choices = []
+
+  /* List of Functions */
+  this.consequence   = []
+}
